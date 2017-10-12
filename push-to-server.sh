f15eaba91edab9e54b0f2ba38578bc9edf1cf67a
Christian Micklisch <christian.micklisch@successwithsos.com>
2017-10-12 11:18:59 -0400

    Cleaned up the push-to-server commands

--- push-to-server.sh
+++ push-to-server.sh
@@ -3,12 +3,12 @@
 # setup ssh information.
 . "helpers/get-ssh-info.sh"
 
-COMMANDS[0]="echo '${SSH_PASSWORD}' | sudo -S rm -rf errorapi"
+COMMANDS[0]="sudo -S rm -rf errorapi"
 COMMANDS[1]="git clone https://bitbucket.org/scooblyboo/errorapi.git"
-COMMANDS[2]="echo '${SSH_PASSWORD}' | sudo rsync -a errorapi/src/ /var/www/error_api/src/"
-COMMANDS[3]="echo '${SSH_PASSWORD}' | sudo rsync -a errorapi/vagrant/ install/"
-COMMANDS[4]="echo '${SSH_PASSWORD}' | sudo rsync -a errorapi/sql/ sql/"
-COMMANDS[5]="echo '${SSH_PASSWORD}' | sudo chown www-data:www-data -R /var/www/error_api"
+COMMANDS[2]="sudo rsync -a errorapi/src/ /var/www/error_api/src/"
+COMMANDS[3]="sudo rsync -a errorapi/vagrant/ install/"
+COMMANDS[4]="sudo rsync -a errorapi/sql/ sql/"
+COMMANDS[5]="sudo chown www-data:www-data -R /var/www/error_api"
 COMMANDS[6]="bash install/composer.sh"
 
 n_elements=${#COMMANDS[@]}
